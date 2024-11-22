ad719d47eb21816ef2f2d9560b0a0a0546a7f359
adaniel105 <91155263+adaniel105@users.noreply.github.com>
2024-11-22 22:01:41 +0000

    lineup

--- bronze/ex-1.cpp
+++ bronze/ex-1.cpp
@@ -395,3 +395,47 @@ int main(){
     cout << ans << endl;
 }
 
+
+//https://usaco.org/index.php?page=viewproblem2&cpid=965
+//bruteforce soln.
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int main() {
+	freopen("lineup.in", "r", stdin);
+	freopen("lineup.out", "w", stdout);
+	cin >> N;
+
+    //extract cow names
+	vector<pair<string, string>> restrictions;
+	for (int i = 0; i < N; i++) {
+		string a, t, b;
+		cin >> a;
+		cin >> t >> t >> t >> t;
+		cin >> b;
+		restrictions.push_back({a, b});
+	}
+
+	vector<string> cows = {"Bessie", "Buttercup", "Belinda", "Beatrice",
+	                       "Bella",  "Blue",      "Betsy",   "Sue"};
+	sort(cows.begin(), cows.end());
+	int count = 0;
+
+
+    //go through all permutations of cows until the distance
+    //between our specified cows is appropriate
+	while (next_permutation(cows.begin(), cows.end())) {
+		bool passed = true;
+		for (auto p : restrictions) {
+			string cow1 = p.first;
+			string cow2 = p.second;
+			auto a = find(cows.begin(), cows.end(), cow1);
+			auto b = find(cows.begin(), cows.end(), cow2);
+			if (abs(a - b) != 1) { passed = false; }
+		}
+		if (passed) { break; }
+	}
+
+	for (auto cow : cows) { cout << cow << endl; }
+}
